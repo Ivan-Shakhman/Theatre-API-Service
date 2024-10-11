1af6e2f3e1719cd73291cd3c76da9055e25673ce
Ivan Shakhman <shakhmanivan@gmail.com>
2024-10-11 20:19:58 +0300

    add models to Presformanse, Reservation, Ticket

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 class TheatreHall(models.Model):
@@ -37,3 +38,20 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+
+class Performance(models.Model):
+    play = models.ForeignKey(Play, related_name="performances", on_delete=models.CASCADE)
+    TheatreHall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    performance = models.ForeignKey(Performance, on_delete=models.CASCADE)
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
