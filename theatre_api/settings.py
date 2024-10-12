b86f1c90a76a3bd6ecd293f3ebb45db8188b5611
Ivan Shakhman <shakhmanivan@gmail.com>
2024-10-12 20:51:14 +0300

    add token authenticate

--- theatre_api/settings.py
+++ theatre_api/settings.py
@@ -139,5 +139,8 @@ REST_FRAMEWORK = {
     "DEFAULT_THROTTLE_RATES": {
         "anon": "10/minute",
         "user": "20/minute",
-    }
+    },
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+ 	    "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
 }
