9add37264bcd287e365bad1cfd64cc41b0a97e4b
Ivan Shakhman <shakhmanivan@gmail.com>
2024-10-12 18:30:04 +0300

    add routers for my views

--- api/views.py
+++ api/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets, mixins
 from rest_framework.viewsets import GenericViewSet
 
-from api.models import Genre, Actor, TheatreHall, Play, Performance
+from api.models import Genre, Actor, TheatreHall, Play, Performance, Reservation
 from api.serializers import GenreSerializer, ActorSerializer, TheatreHallSerializer, PlaySerializer, PlayListSerializer, \
-    PlayDetailSerializer, PerformanceSerializer, PerformanceListSerializer, PerformanceDetailSerializer
+    PlayDetailSerializer, PerformanceSerializer, PerformanceListSerializer, PerformanceDetailSerializer, \
+    ReservationSerializer, ReservationListSerializer
 
 
 class GenreViewSet(
@@ -37,6 +38,7 @@ class TheatreHallViewSet(
 class PlayViewSet(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
     GenericViewSet
 ):
     queryset = Play.objects.prefetch_related("genres", "actors")
@@ -53,6 +55,7 @@ class PlayViewSet(
 class PerformanceViewSet(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
     GenericViewSet
 ):
     queryset = Performance.objects.select_related("theatre_hall", "play")
@@ -66,3 +69,24 @@ class PerformanceViewSet(
         return PerformanceSerializer
 
 
+class ReservationViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet
+):
+    queryset = Reservation.objects.select_related("tickets")
+    serializer_class = ReservationSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ReservationListSerializer
+        if self.action == "retrieve":
+            return PerformanceDetailSerializer
+        return ReservationSerializer
+
+    def get_queryset(self):
+        query_set = self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
