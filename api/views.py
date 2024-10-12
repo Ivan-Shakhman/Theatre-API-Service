21cfdfa157d270819d0e04a26730fea47f6f228e
Ivan Shakhman <shakhmanivan@gmail.com>
2024-10-12 16:17:45 +0300

    create views for actors and theatre hall

--- api/views.py
+++ api/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, mixins
 from rest_framework.viewsets import GenericViewSet
 
-from api.models import Genre
-from api.serializers import GenreSerializer
+from api.models import Genre, Actor, TheatreHall
+from api.serializers import GenreSerializer, ActorSerializer, TheatreHallSerializer
 
 
 class GenreViewSet(
@@ -15,4 +15,20 @@ class GenreViewSet(
     serializer_class = GenreSerializer
 
 
+class ActorViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class TheatreHallViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet
+):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
 
