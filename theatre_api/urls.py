6e6a164414b80fe802601f9b400a7dee59c35a8e
Ivan Shakhman <shakhmanivan@gmail.com>
2024-10-12 21:43:40 +0300

    fix user authorization with jwt token

--- theatre_api/urls.py
+++ theatre_api/urls.py
@@ -21,7 +21,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include('api.urls'), name="api"),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
+    path("api/user/", include('user.urls'), name="user"),
 ]
