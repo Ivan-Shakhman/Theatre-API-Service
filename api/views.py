8760858328655d39ce03e21835df0f6a1414b941
Ivan Shakhman <shakhmanivan@gmail.com>
2024-10-12 16:03:36 +0300

    add view for genres

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from api.models import Genre
+from api.serializers import GenreSerializer
+
+
+class GenreViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
