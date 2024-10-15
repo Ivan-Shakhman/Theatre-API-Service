ff7082c4e189d727486cd1f209fba39e31d6771a
Ivan Shakhman <shakhmanivan@gmail.com>
2024-10-15 17:33:15 +0300

    add migration

--- theatre_api/settings.py
+++ theatre_api/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'theatre_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
